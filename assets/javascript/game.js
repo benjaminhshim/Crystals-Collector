3449c5f97c4d23e5b5a2082a699cbd481412305a
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-03-16 16:13:41 -0700

    Generate random numbers for computer and crystals

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    
+    // COMPUTER GENERATES RANDOM NUMBER
+    var computer = Math.floor((Math.random() * 120) + 19);
+    $('.computer').html(computer);
+
+    // EACH CRYSTAL HOLDS ITS OWN POINT VALUE
+    var bluePoints = Math.floor((Math.random() * 12) + 1);
+    var orangePoints = Math.floor((Math.random() * 12) + 1);
+    var whitePoints = Math.floor((Math.random() * 12) + 1);
+    var greenPoints = Math.floor((Math.random() * 12) + 1);
+
+
+
+
+
+
+
+
+
+
+})
